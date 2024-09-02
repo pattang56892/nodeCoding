bca7268dcd62abdbdc176a85a7b832b6e8d96e5d
PT_backup_Master <https://github.com/pattang56892/PT_backup_Master.git>
2024-09-02 14:14:47 -0400

    debugging card detail format

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,41 +1,11 @@
+// method #1
 module.exports = (temp, topic) => {
   let output = temp.replace(/{%TOPICNAME%}/g, topic.productName);
   output = output.replace(/{%DETAILED_EXPLANATION%}/g, topic.detailedExplanation);
   output = output.replace(/{%SUMMARY%}/g, topic.description);
-  output = output.replace(/{%ID%}/g, topic.id);
-
-  // Replace codesnippet placeholder
-  output = output.replace(/{%CODESNIPPET%}/g, topic.codesnippet);
-
-  // Optionally, you can remove the {%TAGS%} placeholder if it's still in the template
+  output = output.replace(/{%ID%}/g, topic.id);  
+  output = output.replace(/{%CODE_SNIPPET%}/g, topic.codesnippet);  
   output = output.replace(/{%TAGS%}/g, '');
 
   return output;
 };
-
-
-
-// const fs = require('fs');
-
-// // Load the data from JSON
-// const data = JSON.parse(fs.readFileSync('./dev-data/data.json', 'utf-8'));
-
-// // Function to replace placeholders with actual data
-// function replaceTemplate(temp, topic) {
-//     let output = temp.replace(/{%TOPICNAME%}/g, topic.productName);
-//     output = output.replace(/{%DETAILED_EXPLANATION%}/g, topic.detailedExplanation);
-//     output = output.replace(/{%CODE_SNIPPET%}/g, topic.codesnippet);
-//     return output;
-// }
-
-// // Load the template (this should point to your existing template file)
-// const templatePath = './templates/template-topic-detail.html';
-// const template = fs.readFileSync(templatePath, 'utf-8');
-
-// // Replace the placeholders
-// const filledTemplate = replaceTemplate(template, data[0]);
-
-// // Overwrite the same template with the filled content
-// fs.writeFileSync(templatePath, filledTemplate);
-
-// console.log("Template processing completed and template updated.");
